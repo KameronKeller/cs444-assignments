a633aab582f8c79524dd5662ae479cce35714343
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-06 16:47:49 -0700

    Test image open and close

--- project05/simfs_test.c
+++ project05/simfs_test.c
@@ -1,16 +1,48 @@
-# create_image_file()
+#include <stdio.h>
+#include "image.h"
+#include "ctest.h"
 
-# read_block()
+// # create_image_file()
 
-# write_block()
+// # read_block()
 
-# locate_free_block()
+// # write_block()
 
-# toggle_free()
+// # locate_free_block()
 
-# locate_free_inode()
+// # toggle_free()
+
+// # locate_free_inode()
+
+#ifdef CTEST_ENABLE
+
+void test_image_open_and_close(void)
+{
+	int image_fd = image_open("test", 0);
+	CTEST_ASSERT(image_fd == 3, "Test create and open new image");
+	CTEST_ASSERT(image_close() == 0, "Test closing and image file");
+
+	image_fd = image_open("test", 1);
+	CTEST_ASSERT(image_fd == 3, "Test create and truncate new image");
+	CTEST_ASSERT(image_close() == 0, "Test closing truncated file");
+}
 
 int main(void)
 {
-	
+    CTEST_VERBOSE(1);
+
+	test_image_open_and_close();
+
+    CTEST_RESULTS();
+
+    CTEST_EXIT();
 }
+
+#else
+
+int main(void)
+{
+    printf("Running normally!\n");
+}
+
+#endif
