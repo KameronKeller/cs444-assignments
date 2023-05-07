3ca500db2f3cecb0db986721fa1f9854626e760a
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-06 21:40:45 -0700

    Test block write and read and fix bugs

--- project05/block.c
+++ project05/block.c
@@ -6,11 +6,25 @@
 
 # define BLOCK_SIZE 4096 // 4 KB block size
 
-unsigned char *bread(int block_num, unsigned char *block)
+off_t get_block_location(int block_num)
 {
 	off_t offset = block_num * BLOCK_SIZE;
 	off_t block_location = lseek(image_fd, offset, SEEK_SET);
-	int bytes_read = read(block_location, block, BLOCK_SIZE);
+	return block_location;
+}
+
+unsigned char *bread(int block_num, unsigned char *block)
+{
+	// off_t offset = block_num * BLOCK_SIZE;
+	// off_t block_location = lseek(image_fd, offset, SEEK_SET);
+	off_t block_location = get_block_location(block_num);
+	// printf("bread %lld\n", block_location);
+	// printf("fd %d\n", image_fd);
+	if (block_location == -1) {
+		perror("bread(): File descriptor offset failed\n");
+		exit(1);
+	}
+	int bytes_read = read(image_fd, block, BLOCK_SIZE);
 	if (bytes_read == -1) {
 		perror("Block read failed\n");
 		exit(1);
@@ -21,13 +35,16 @@ unsigned char *bread(int block_num, unsigned char *block)
 
 void bwrite(int block_num, unsigned char *block)
 {
-	off_t offset = block_num * BLOCK_SIZE;
-	off_t block_location = lseek(image_fd, offset, SEEK_SET);
+	// off_t offset = block_num * BLOCK_SIZE;
+	// off_t block_location = lseek(image_fd, offset, SEEK_SET);
+	off_t block_location = get_block_location(block_num);
+	// printf("bwrite %lld\n", block_location);
+	// printf("fd %d\n", image_fd);
 	if (block_location == -1) {
-		perror("File descriptor offset failed\n");
+		perror("bwrite(): File descriptor offset failed\n");
 		exit(1);
 	}
-	int bytes_written = write(image_fd, block, 4);
+	int bytes_written = write(image_fd, block, BLOCK_SIZE);
 	if (bytes_written == -1) {
 		perror("Block write failed\n");
 		exit(1);
--- project05/simfs_test.c
+++ project05/simfs_test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include "image.h"
+#include <string.h>
 #include "ctest.h"
+#include "image.h"
+#include "block.h"
 
 // # create_image_file()
 
@@ -27,11 +29,33 @@ void test_image_open_and_close(void)
 	CTEST_ASSERT(image_close() == 0, "Test closing truncated file");
 }
 
+void test_block_write_and_read(void)
+{
+	int block_num = 0;
+	const int num_bytes = 4096;
+	unsigned char test_data[num_bytes] = {92}; // Initialize array block with zeros
+		// // printf("test_data: %d\n", test_data[2]);
+	image_open("test", 0); // Open the file
+	bwrite(block_num, test_data);
+
+	unsigned char read_data[num_bytes];
+	bread(block_num, read_data);
+	// // printf("return_data: %d\n", return_data[2]);
+	int diff = memcmp(test_data, read_data, num_bytes);
+	printf("diff: %d\n", diff);
+
+	CTEST_ASSERT(memcmp(test_data, read_data, 3) == 0, "Test bwrite and bread");
+	image_close();
+
+
+}
+
 int main(void)
 {
     CTEST_VERBOSE(1);
 
 	test_image_open_and_close();
+	test_block_write_and_read();
 
     CTEST_RESULTS();
 
