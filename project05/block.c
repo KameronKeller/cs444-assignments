a80eabd5e508cdcb1f2604f70bdf6e4e978fc8fc
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-06 19:31:37 -0700

    Begin bread and bwrite implementation

--- project05/block.c
+++ project05/block.c
@@ -1,2 +1,30 @@
-unsigned char *bread(int block num, unsigned char *block)
-void bwrite(int block num, unsigned char *block)
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "block.h"
+#include "image.h"
+
+# define BLOCK_SIZE 4096 // 4 KB block size
+
+unsigned char *bread(int block_num, unsigned char *block)
+{
+	off_t offset = block_num * BLOCK_SIZE;
+	off_t block_location = lseek(image_fd, offset, SEEK_SET);
+	int bytes_read = read(block_location, block, BLOCK_SIZE);
+
+	return block;
+}
+
+void bwrite(int block_num, unsigned char *block)
+{
+	off_t offset = block_num * BLOCK_SIZE;
+	off_t block_location = lseek(image_fd, offset, SEEK_SET);
+	printf("%lld\n", block_location);
+	int bytes_written = write(image_fd, block, BLOCK_SIZE);
+	if (bytes_written == -1) {
+		printf("Failed to write\n");
+		exit(1);
+	}
+	// printf("%d\n", bytes_written);
+
+}
