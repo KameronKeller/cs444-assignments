181713f7e68d75734f3360d7acd545c881e02365
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-06 21:46:26 -0700

    Define block size in header file

--- project05/block.h
+++ project05/block.h
@@ -1,6 +1,8 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+# define BLOCK_SIZE 4096 // 4 KB block size
+
 unsigned char *bread(int block_num, unsigned char *block);
 void bwrite(int block_num, unsigned char *block);
 
