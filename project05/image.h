48a85e700ea3bcf6ccb78f82c8cd7f1d72664497
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-07 17:14:14 -0700

    Add macros for truncate, read, write, create

--- project05/image.h
+++ project05/image.h
@@ -1,6 +1,9 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#define TRUNCATE 1 // Flag for truncating the file
+#define READ_WRITE 0 // Flag for reading, writing, and/or creating the file if it doesn't exit
+
 int image_open(char *filename, int truncate);
 int image_close(void);
 
