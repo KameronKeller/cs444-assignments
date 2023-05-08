8257de3530fd933f28ae28b75233daa9bc8ca303
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-07 18:08:06 -0700

    Implement ialloc

--- project05/inode.c
+++ project05/inode.c
@@ -1,1 +1,25 @@
+#include "inode.h"
+#include "block.h"
+#include "free.h"
+
 int ialloc(void)
+{
+	// Create a buffer block
+	unsigned char block[BLOCK_SIZE];
+	// Read to the block
+	bread(INODE_MAP, block);
+	// Find the free location
+	int num = find_free(block);
+
+	// If no free location, return 0
+	if (num == 0) {
+		return FAILURE;
+	} else {
+		// mark num as IN_USE
+		set_free(block, num, IN_USE);
+		// Write to the block
+		bwrite(num, block);
+		return num;
+	}
+
+}
