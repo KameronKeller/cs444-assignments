f66c6372ab1fd22b2dad2f1513e296ba3872bad1
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-07 17:59:27 -0700

    Refactor functions and add macros

--- project05/free.c
+++ project05/free.c
@@ -2,15 +2,14 @@
 #include "free.h"
 #include "block.h"
 
-# define BYTE 8
 
 int find_low_clear_bit(unsigned char x)
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < BYTE; i++)
         if (!(x & (1 << i)))
             return i;
     
-    return -1;
+    return FAILURE;
 }
 
 int calculate_block_number(int index, int bit_num)
@@ -20,9 +19,9 @@ int calculate_block_number(int index, int bit_num)
 
 void set_free(unsigned char *block, int num, int set)
 {
-	int byte_num = num / 8;  // 8 bits per byte
-	int bit_num = num % 8;
-	if (set == 1) {
+	int byte_num = num / BYTE;
+	int bit_num = num % BYTE;
+	if (set == IN_USE) {
 		block[byte_num] |= set << bit_num;
 	} else {
 		block[byte_num] &= ~(1 << bit_num);
@@ -32,16 +31,15 @@ void set_free(unsigned char *block, int num, int set)
 
 int find_free(unsigned char *block)
 {
-	// int size = sizeof(block) / sizeof(block[0]);
-	// printf("size: %d\n", size);
+	// Loop over each byte in the block
 	for (int i = 0; i < BLOCK_SIZE; i ++) {
 		unsigned char x = block[i];
 
+		// Find a clear bit
 		int bit_num = find_low_clear_bit(x);
-			// printf("byte: %d\n", i);
-			// printf("\tbit_num: %d\n", bit_num);
-		if (bit_num != -1) {
 
+		// If search succeeds, return the bit number
+		if (bit_num != FAILURE) {
 			return calculate_block_number(i, bit_num);
 		}
 	}
