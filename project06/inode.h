7d6ed3182a17b0974112b2a91387573fbab960da
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-15 09:00:38 -0700

    Add struct inode and macros

--- project06/inode.h
+++ project06/inode.h
@@ -4,6 +4,22 @@
 #define INODE_MAP 1
 #define FAILURE -1
 
+#define INODE_PTR_COUNT 16
+
+#define MAX_SYS_OPEN_FILES 64
+
+struct inode {
+    unsigned int size;
+    unsigned short owner_id;
+    unsigned char permissions;
+    unsigned char flags;
+    unsigned char link_count;
+    unsigned short block_ptr[INODE_PTR_COUNT];
+
+    unsigned int ref_count;  // in-core only
+    unsigned int inode_num;
+};
+
 int ialloc(void);
 
 #endif
