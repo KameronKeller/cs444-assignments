48377223b7db46216c681a7f1a8a1233a5c0a53d
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-18 10:50:53 -0700

    Add function header

--- project06/inode.h
+++ project06/inode.h
@@ -8,7 +8,17 @@
 
 #define MAX_SYS_OPEN_FILES 64
 
+#define BLOCK_SIZE 4096
 #define INODE_SIZE 64
+#define INODE_FIRST_BLOCK 3
+
+#define INODES_PER_BLOCK (BLOCK_SIZE / INODE_SIZE)
+
+#define OWNER_ID_OFFSET 4
+#define PERMISSIONS_OFFSET OWNER_ID_OFFSET + 2
+#define FLAGS_OFFSET PERMISSIONS_OFFSET + 1
+#define LINK_COUNT_OFFSET FLAGS_OFFSET + 1
+#define BLOCK_POINTER_OFFSET LINK_COUNT_OFFSET + 1
 
 struct inode {
     unsigned int size;
@@ -27,5 +37,7 @@ struct inode *find_incore_free(void);
 struct inode *find_incore(unsigned int inode_num);
 void read_inode(struct inode *in, int inode_num);
 void write_inode(struct inode *in);
+void clear_incore_inodes(void);
+struct inode *iget(int inode_num);
 
 #endif
