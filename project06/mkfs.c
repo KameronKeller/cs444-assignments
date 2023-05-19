3dfe3ddeb51c52cea6b95f1fd60f524b2965da9d
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-19 13:06:00 -0700

    Use memset instead of helper function

--- project06/mkfs.c
+++ project06/mkfs.c
@@ -4,17 +4,11 @@
 #include "block.h"
 #include "mkfs.h"
 
-void initialize_file(unsigned char *block, int value, int size) {
-	for (int i = 0; i < size; i++) {
-		block[i] = value;
-	}
-}
 
 void mkfs(void)
 {
 	unsigned char initialize_data[FOUR_MB_IMAGE];
 	memset(initialize_data, 0, FOUR_MB_IMAGE);
-	// initialize_file(initialize_data, ZEROS, FOUR_MB_IMAGE);
 
 	write(image_fd, initialize_data, FOUR_MB_IMAGE);
 
