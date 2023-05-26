8728cc0fe3fcec0b478eb783589c54d3be7c576e
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-26 12:19:04 -0700

    mkfs now creates root directory

--- project07/mkfs.c
+++ project07/mkfs.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include "image.h"
 #include "block.h"
 #include "mkfs.h"
+#include "inode.h"
+#include "pack.h"
 
 
 void mkfs(void)
@@ -15,5 +18,31 @@ void mkfs(void)
 	for (int i = 0; i < METADATA; i++) {
 		alloc();
 	}
+
+	// Allocate the root inode and directory block
+	struct inode *root_inode = ialloc();
+	int directory_block = alloc();
+
+	// Initialize the root inode
+	root_inode->flags = DIRECTORY_FLAG;
+	root_inode->size = ROOT_DIR_SIZE;
+	root_inode->block_ptr[0] = get_block_location(directory_block);
+
+	// Create an empty block to store the directory information
+	unsigned char block[BLOCK_SIZE];
+
+	// Write the "." file to the block
+	write_u16(block, root_inode->inode_num);
+	strcpy((char *)block + FILE_NAME_OFFSET, ".");
+
+	// Write the ".." file to the block
+	write_u16(block + FIXED_LENGTH_RECORD_SIZE, root_inode->inode_num);
+	strcpy((char *)block + FILE_NAME_OFFSET + FIXED_LENGTH_RECORD_SIZE, "..");
+
+	// Write the block to disk
+	bwrite(directory_block, block);
+
+	// Write the inode to disk
+	iput(root_inode);
 
 }
--- project07/mkfs.h
+++ project07/mkfs.h
@@ -4,6 +4,10 @@
 #define FOUR_MB_IMAGE 4096*1024
 #define ZEROS 0
 #define METADATA 7
+#define DIRECTORY_FLAG 2
+#define FIXED_LENGTH_RECORD_SIZE 32
+#define ROOT_DIR_SIZE FIXED_LENGTH_RECORD_SIZE*2
+#define FILE_NAME_OFFSET 2
 
 void mkfs(void);
 
