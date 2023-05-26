809698345ddac9cdcc186d4bf2edad2afb5a3715
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-26 16:13:14 -0700

    Implement directory close and add annotations

--- project07/directory.c
+++ project07/directory.c
@@ -19,6 +19,7 @@ struct directory *directory_open(int inode_num)
 
     // Malloc space for the directory
     struct directory *open_directory = malloc(sizeof(struct directory));
+
     // Initialize directory
     open_directory->inode = directory_inode;
     open_directory->offset = 0;
@@ -29,20 +30,35 @@ struct directory *directory_open(int inode_num)
 
 int directory_get(struct directory *dir, struct directory_entry *ent)
 {
+    // Check if the offset is valid
     unsigned int offset = dir->offset;
     unsigned int size = dir->inode->size;
     if (offset >= size) {
         return -1;
     }
 
+    // Calculate the block index and block number
     int data_block_index = offset / BLOCK_SIZE;
     short data_block_num = dir->inode->block_ptr[data_block_index] / BLOCK_SIZE;
 
+    // Read the block to a block array
     unsigned char block[BLOCK_SIZE];
     bread(data_block_num, block);
 
+    // Calculate the offset within the block
     int offset_in_block = offset % BLOCK_SIZE;
+
+    // Read the inode number
     ent->inode_num = read_u16(block + offset_in_block);
+
+    // Copy the file name
     strcpy(ent->name, (char *)block + FILE_NAME_OFFSET);
+
     return 0;
+}
+
+void directory_close(struct directory *d)
+{
+    iput(d->inode);
+    free(d);
 }
