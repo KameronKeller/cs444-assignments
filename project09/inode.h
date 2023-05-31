782b84bdf37efbe2f5cf61e5a42d87380cf71be1
Kameron Keller <mrkameronkeller@gmail.com>
2023-05-31 08:40:29 -0700

    Add function prototype

--- project09/inode.h
+++ project09/inode.h
@@ -43,5 +43,6 @@ void read_inode(struct inode *in, int inode_num);
 void write_inode(struct inode *in);
 struct inode *iget(int inode_num);
 void iput(struct inode *in);
+struct inode *namei(char *path);
 
 #endif
